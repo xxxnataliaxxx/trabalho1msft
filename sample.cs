3a16e5592cbe0e5177c41165bf21395aa92940d1
Natalia Gabriel <natalia.gabriel@alumni.usp.br>
2022-09-02 20:35:32 -0300

    adding c# code to repository

---
+++ sample.cs
@@ -0,0 +1,12 @@
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a number:");
+            number = Convert.ToInt32(Console.ReadLine());
+ 
+            Console.WriteLine("You entered :{0}",number);
+            Console.ReadLine();
+        }
+    }
